6a9357cb72a0dcfb3fed3e1700452dd1ad2dc633
mario-sanmartin <sanmartin.mario@outlook.com>
2020-08-19 01:56:30 -0400

    Repasando Vue, a derribar la muralla

---
+++ src/components/MiComponente.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <p>Mi primer impotente</p>
+    <!-- <p v-if="condicion">Este mensaje se mostrara</p> -->
+    <ul v-if="condicion">
+      <li>Madara el mas picante de todos</li>
+      <li>El papi del caos , sin lugar a dudas.</li>
+    </ul>
+
+    <p v-else-if="list.length !== 5 && list[0] === 0">
+      Este mensaje esta maldito
+    </p>
+    <p v-else>
+      Este si mensaje si que esta maldito loco!!!
+    </p>
+    <img :src="pic" :class="poto" />
+
+    <div :style="estiloso">
+      <ul>
+        <li v-for="(item, i) in weichafes" :key="i">
+          id:
+          {{ item.id }}
+          -
+          {{ item.name }} | Poderes:
+          <span v-for="(poder, k) in item.poderes" :key="`podercito` + k">
+            {{ poder }}
+          </span>
+        </li>
+      </ul>
+    </div>
+
+    <!-- {{ miVariable }} -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MiComponente",
+  data() {
+    return {
+      miVariable: "Hola Madara Uchija",
+      condicion: false,
+      list: [0, 2, 3, 4, 5],
+      weichafes: [
+        { name: "Lautaro", id: 1, poderes: ["tierra", "aire"] },
+        { name: "Kalfukura", id: 2, poderes: ["susano", "perfecto"] },
+        { name: "Caupolican", id: 3, poderes: ["perro", "kk"] }
+      ],
+      pic: "https://i.imgur.com/BBcy6Wc.jpg",
+      poto: {
+        active: true,
+        "text-danger": false
+      },
+      estiloso: {
+        background: "red",
+        width: "300px",
+        height: "30px",
+        fontSize: "20px"
+      }
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <mi-componente /> kebab-kase -->
+    <MiComponente />
+    <!--Pascal Case,
+    CamelCase es la primera miniscula y la siguiente
+    palabra en mayuscula-->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import MiComponente from "@/components/MiComponente.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+    MiComponente
   }
 };
 </script>
